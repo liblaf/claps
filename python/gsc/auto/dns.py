5d0c0f50f4d22e095a74447a3ed22520bdeb7cad
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-05 23:26:59 +0800

    fix(gfw/sub/conf): fix handling of DNS queries in the auto DNS module
    Imported the necessary `parse` module from `urllib` to properly parse the server URL in the `query` function. Updated the `query` function to handle both `https` and `udp` schemes when making DNS queries. Added error handling in the `dns` function to log any exceptions that occur during the DNS query process.
    
    This change improves the reliability and flexibility of DNS query handling in the auto DNS module.

--- python/gsc/auto/dns.py
+++ python/gsc/auto/dns.py
@@ -1,5 +1,6 @@
 import asyncio
 import functools
+from urllib import parse
 
 from dns import asyncquery as _query
 from dns import message as _message
@@ -15,6 +16,12 @@ SERVERS: list[str] = [
 
 @functools.cache
 async def dns() -> str:
+    server: str = "udp://127.0.0.1:60053"
+    try:
+        await query(server)
+        return server
+    except Exception as e:
+        logger.error("DNS {}: {}", server, e)
     for task in asyncio.as_completed([query(server) for server in SERVERS], timeout=5):
         try:
             result: str = await task
@@ -29,10 +36,14 @@ async def dns() -> str:
 @logger.catch
 async def query(server: str, query: str = "chat.liblaf.me") -> str:
     q: _message.QueryMessage = _message.make_query(qname=query, rdtype="A")
-    if server.startswith("https://"):
-        await _query.https(q=q, where=server, timeout=5)  # type: ignore
-        return server
-    raise NotImplementedError()
+    parsed: parse.ParseResult = parse.urlparse(server)
+    match parsed.scheme:
+        case "https":
+            await _query.https(q=q, where=server, timeout=5)  # type: ignore
+            return server
+        case "udp" | _:
+            await _query.udp(q=q, where=parsed.hostname, port=parsed.port, timeout=5)  # type: ignore
+            return server
 
 
 BEST: str = asyncio.run(dns())
