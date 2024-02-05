ff80e516b85a45c201d8640480c07f1f080b1076
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-05 16:51:32 +0800

    fix(ddns/dns/install): update ddns service executable path
    Update the executable path for the ddns service in the systemd unit file to include the 'dns' subcommand. This change ensures that the correct command is executed when starting the ddns service.

---
+++ src/bin/ddns/cmd/dns/install.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use clap::Args;
+use directories::BaseDirs;
+
+use claps::common::log::LogResult;
+
+#[derive(Args)]
+pub struct Cmd {}
+
+impl Cmd {
+    pub async fn run(self) -> Result<()> {
+        let dirs = BaseDirs::new().unwrap();
+        let dir = dirs.config_dir();
+        let contents = include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/docs/ddns/ddns.service"
+        ));
+        let path = dir.join("systemd/user/ddns.service");
+        tokio::fs::write(path.as_path(), contents).await.log()?;
+        tracing::info!("installed: {:?}", path);
+        let path = dir.join("systemd/user/ddns.timer");
+        let contents = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/docs/ddns/ddns.timer"));
+        tokio::fs::write(&path, contents).await.log()?;
+        tracing::info!("installed: {:?}", path);
+        claps::external::run(["systemctl", "--user", "enable", "--now", "ddns.timer"]).await?;
+        tracing::info!("systemctl --user enable --now ddns.timer");
+        Ok(())
+    }
+}
--- src/bin/ddns/cmd/dns/mod.rs
+++ src/bin/ddns/cmd/dns/mod.rs
@@ -5,6 +5,7 @@ use claps::api::cloudflare::zones::Zones;
 use super::CloudflareArgs;
 
 mod delete;
+mod install;
 mod list;
 mod update;
 
@@ -20,6 +21,7 @@ impl Cmd {
     pub async fn run(self) -> Result<()> {
         match self.cmd {
             SubCmd::Delete(cmd) => cmd.run().await,
+            SubCmd::Install(cmd) => cmd.run().await,
             SubCmd::List(cmd) => cmd.run().await,
             SubCmd::Update(cmd) => cmd.run().await,
         }
@@ -29,6 +31,7 @@ impl Cmd {
 #[derive(Subcommand)]
 enum SubCmd {
     Delete(delete::Cmd),
+    Install(install::Cmd),
     List(list::Cmd),
     Update(update::Cmd),
 }
