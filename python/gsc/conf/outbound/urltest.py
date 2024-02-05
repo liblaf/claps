70be8e9011bce03cbdd0f52e30803ad4694db7f8
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-06 02:35:16 +0800

    fix(gfw/sub/conf): add optional url field to URLTest model
    Import the `Optional` type from the `typing` module to allow the `url` field in the `URLTest` model to be nullable. Set a default value of "https://cp.cloudflare.com" for the `url` field. This change enhances the flexibility of the `URLTest` model by making the `url` field optional.

--- python/gsc/conf/outbound/urltest.py
+++ python/gsc/conf/outbound/urltest.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 import pydantic
 
@@ -8,6 +8,7 @@ class URLTest(pydantic.BaseModel):
     tag: str
 
     outbounds: list[str] = pydantic.Field(default_factory=list)
+    url: Optional[str] = "https://cp.cloudflare.com"
 
     def __hash__(self) -> int:
         return hash(self.tag)
